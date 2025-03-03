fd7fcf738a66adc7a83036e3a01c7cd3553f05c9
sairam <r.sairam514@gmail.com>
2025-03-03 21:43:42 +0530

    adding catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -8,3 +8,20 @@ if [ $ID -ne 0 ]
   else 
      echo " you are root user"
 fi  
+
+dnf module disable nodejs -y
+if ($? -ne 0)
+   then
+     echo "error"
+     exit 1
+   else 
+     echo "success"
+ fi    
+dnf module enable nodejs:18 -y
+if ($? -ne 0)
+   then
+     echo "error"
+     exit 1
+   else 
+     echo "success"
+ fi   
