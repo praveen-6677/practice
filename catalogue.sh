9c02e4c8846047dfb5fc2c320a93d7895d5cdc64
sairam <r.sairam514@gmail.com>
2025-03-03 22:12:48 +0530

    adding catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -17,3 +17,60 @@ if  [ $? -ne 0 ]
    else 
      echo "success"
 fi 
+useradd roboshop
+dnf install nodejs -y
+if  [ $? -ne 0 ]
+  then
+   echo "error"
+   exit 1
+  else 
+     echo "success"
+fi     
+ mkdir -p  /app
+ if  [ $? -ne 0 ]
+   then
+     echo "error"
+     exit 1
+   else 
+     echo "success"
+ fi    
+     
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+ if  [ $? -ne 0 ]
+   then
+     echo "error"
+     exit 1
+   else 
+     echo "success"
+ fi    
+ cd /app 
+ if  [ $? -ne 0 ]
+   then
+     echo "error"
+     exit 1
+   else 
+     echo "success"
+ fi  
+ unzip /tmp/catalogue.zip
+ if  [ $? -ne 0 ]
+   then
+     echo "error"
+     exit 1
+   else 
+     echo "success"
+ fi  
+cd /app
+if  [ $? -ne 0 ]
+   then
+     echo "error"
+     exit 1
+   else 
+     echo "success"
+ fi  
+ npm install 
+ if  [ $? -ne 0 ]
+   then
+     echo "error"
+     exit 1
+   else 
+     echo "success"
